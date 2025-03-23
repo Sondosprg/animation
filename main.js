26b3b723dca6f2447e6d46fb119055afb32ea6c2
snds_cd <sondos23cv@gmail.com>
2025-03-23 11:42:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+let soundous = document.getElementById('soundous');
+let abd = document.getElementById('abd');
+
+// 2 array
+let arr = ['s', 'o', '.', 'l', 'o', 'g', 'i', 'c', '0', '3'];
+let arr1 = ['a', 'b', 'd', 'u', 'a', 'l', 'r', 'o', 'a', 'f'];
+
+
+
+// show letter with time
+// soundous
+
+setTimeout(function () {
+    soundous.innerText = arr[0];
+}, 1000);
+
+setTimeout(function () {
+    soundous.innerText += arr[1];
+}, 2000);
+
+setTimeout(function () {
+    soundous.innerText += arr[2];
+}, 3000);
+
+setTimeout(function () {
+    soundous.innerText += arr[3];
+}, 4000);
+
+setTimeout(function () {
+    soundous.innerText += arr[4];
+}, 5000);
+
+setTimeout(function () {
+    soundous.innerText += arr[5];
+}, 6000);
+
+setTimeout(function () {
+    soundous.innerText += arr[6];
+}, 7000);
+setTimeout(function () {
+    soundous.innerText += arr[7];
+}, 8000);
+setTimeout(function () {
+    soundous.innerText += arr[8];
+}, 9000);
+setTimeout(function () {
+    soundous.innerText += arr[9];
+}, 10000);
+
+
+// abdualroaf
+setTimeout(function () {
+    abd.innerText = arr1[0];
+}, 1000);
+
+setTimeout(function () {
+    abd.innerText += arr1[1];
+
+
+
+}, 2000);
+
+setTimeout(function () {
+    abd.innerText += arr1[2];
+
+}, 3000);
+
+setTimeout(function () {
+    abd.innerText += arr1[3];
+}, 4000);
+
+setTimeout(function () {
+    abd.innerText += arr1[4];
+}, 5000);
+
+setTimeout(function () {
+    abd.innerText += arr1[5];
+}, 6000);
+
+setTimeout(function () {
+    abd.innerText += arr1[6];
+}, 7000);
+setTimeout(function () {
+    abd.innerText += arr1[7];
+}, 8000);
+setTimeout(function () {
+    abd.innerText += arr1[8];
+}, 9000);
+setTimeout(function () {
+    abd.innerText += arr1[9];
+}, 10000);
+
+
+
+
+
+
+
